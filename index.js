cf6b9b6f7d7dc1f2ab043c319537f020a1dc57a5
neo <braun@nebuchadnezzar.local>
2014-11-01 14:39:31 -0400

    solved; but still not formatted correctly;

--- index.js
+++ index.js
@@ -8,6 +8,72 @@ angular
 
 angular
   .module('eg.hsm',[
+    'eg.hsm.root',
     'ui.router'
   ])
 ;
+
+angular.module('eg.hsm.root',['ui.router'])
+  .config($EgHsmRootConfig)
+  .run($EgHsmRootRunner)
+;
+
+$EgHsmRootConfig.$inject = ['$stateProvider','$urlRouterProvider'];
+function $EgHsmRootConfig($stateProvider,$urlRouterProvider) {
+  $urlRouterProvider
+    .otherwise('/root')
+  ;
+  $stateProvider
+    .state('root',{
+      url: '/root',
+      controller: function($scope,$state) {
+        $scope.submit = function($event) {
+          var action = angular
+            .element($event.srcElement)
+            .attr('data-eg-action')
+          ;
+          $state.go('root.child',{p0:$scope.p0});
+        }
+      },
+      templateProvider: ['$q','$http',function($q,$http){
+        var d = $q.defer();
+        d.notify('GET /views/root.html');
+        $http
+          .get('/views/root.html')
+          .success(function(response){
+            d.resolve(response);
+          })
+          .error(function(error){
+            d.reject(error);
+            throw new Error(error);
+          })
+        ;
+        return d.promise;
+      }]
+    })
+    .state('root.child',{
+      url: '^/root/child?p0',
+      templateProvider: ['$q','$http','$stateParams',function($q,$http,$stateParams){
+        var d = $q.defer();
+        d.notify('POST /root/child');
+        $http
+          .post('/root/child',$stateParams)
+          .success(function(response){
+            d.resolve(response);
+          })
+          .error(function(error){
+            d.reject(error);
+            throw new Error(error);
+          })
+        ;
+        return d.promise;
+      }]
+    })
+  ;
+}
+
+$EgHsmRootRunner.$inject = ['$rootScope','$state','$stateParams'];
+function $EgHsmRootRunner($rootScope,$state,$stateParams) {
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+}
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express'),
   server = express(),
+  bodyParser = require('body-parser'),
   port = process.env.PORT || 30000,
   staticRoot = __dirname;
 
 server.use('/', express.static(staticRoot));
+server.use(bodyParser.urlencoded({entended:false}));
+server.post('/root/child',function(q,r){
+  var markup = '';
+  markup += '<h3>child</h3>';
+  markup += '<pre>'+q.body+'</pre>';
+  r.send(markup);
+});
 server.listen(port);
 console.log('http://localhost:'+port);
