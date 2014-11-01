d15ecb49858e645de0200d69d1ef4b9f522de421
neo <braun@nebuchadnezzar.local>
2014-11-01 12:51:12 -0400

    added basic files

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 angular
-	.module('app',[])
+	.module('app',['eg.hsm'])
 	.provider('appProvider',[function(){this.$get=[function(){return new appProvider();}];function appProvider(){}}])
 	.service('appService',[function(){return{};}])
 	.factory('appFactory',[function(){return{};}])
 	.directive('appDirective',[function(){return{};}])
+;
+
+angular
+  .module('eg.hsm',[
+    'ui.router'
+  ])
 ;
